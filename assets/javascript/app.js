baf26553b4f3b0813e2feb623a78e275e7ce367e
Esar Behlum <esarnb@gmail.com>
2019-06-06 23:19:14 -0700

    New Train listener and helper functions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,3 +8,33 @@ var firebaseConfig = {
     messagingSenderId: "721920220550",
     appId: "1:721920220550:web:408403b22546d5a2"
 };
+
+//Global Variables
+var trainName = $("#train-name-input");
+var destination = $("#destination-input");
+var firstTrainTime = $("#ftt-input");
+var frequency = $("#frequency-input");
+var submit = $("#add-train-btn");
+
+//Functions
+function nextArrival() {
+
+}
+
+function minutesAway() {
+
+}
+
+
+//New Train Event Listener
+database.ref().on("child_added", function(childSnapshot) {
+    // Log everything that's coming out of snapshot
+    console.log(
+        `Name: ${childSnapshot.val().trainName} |`+
+        `Destination: ${childSnapshot.val().destination} |`+
+        `FirstTrainTime: ${childSnapshot.val().firstTrainTime} |`+
+        `Frequency: ${childSnapshot.val().frequency} |`
+    )
+}, function(errorObject) {
+    console.log("Errors handled: " + errorObject.code);
+});
