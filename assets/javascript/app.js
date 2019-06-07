17c42f32313042059f9dba7a3471a3a92ffab700
Esar Behlum <esarnb@gmail.com>
2019-06-06 23:08:34 -0700

    Initial files with CDNs and stylesheet

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+//FireBase Initialization
+var firebaseConfig = {
+    apiKey: "AIzaSyDkeFLoKhW5wVDFYMTf7DoICGL14Vnljj8",
+    authDomain: "trainscheduler-sr.firebaseapp.com",
+    databaseURL: "https://trainscheduler-sr.firebaseio.com",
+    projectId: "trainscheduler-sr",
+    storageBucket: "trainscheduler-sr.appspot.com",
+    messagingSenderId: "721920220550",
+    appId: "1:721920220550:web:408403b22546d5a2"
+};
